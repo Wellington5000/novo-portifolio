6216945b8d8e0401b3ec032a6889f1fc3a078bbf
Wellington5000 <wellingtonsantos5000@gmail.com>
2024-08-21 23:39:44 -0300

    background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimationOptions } from 'ngx-lottie';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  lottieOptions: AnimationOptions = {
+    path: '/assets/json/background-2.json',
+  }
+
   title = 'wt-portifolio';
 }
